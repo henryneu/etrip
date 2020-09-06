04b6aa88a43ccad2ae689c4e029d8d8654ac9780
zhouzhenhua <zhouzhenhua602747@pwrd.com>
2020-09-06 21:29:09 +0800

    替换widget

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("搜索"),
+    return Scaffold(
+      body: Center(
+        child: Text('搜索'),
+      ),
     );
   }
 }
--- lib/pages/trip_page.dart
+++ lib/pages/trip_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class TripPage extends StatelessWidget {
+class TripPage extends StatefulWidget {
+  @override
+  _TripPageState createState() => _TripPageState();
+}
+
+class _TripPageState extends State<TripPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("旅拍"),
+    return Scaffold(
+      body: Center(
+        child: Text('旅拍'),
+      ),
     );
   }
 }
